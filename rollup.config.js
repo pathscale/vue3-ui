d1871e6dc311cbe4334947f125e6c0db01b599bd
Brett Zamir <brettz9@yahoo.com>
2020-06-13 15:59:24 +0800

    - Add ESM browser distribution and examples (for rapid prototyping/debugging) - npm: Update devDeps

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,12 @@
 import vue from 'rollup-plugin-vue'
-import postcss from 'rollup-plugin-postcss'
-import resolve from '@rollup/plugin-node-resolve';
-
-// import typescript from '@rollup/plugin-typescript'
-// import typescript from 'rollup-plugin-typescript'
 // See https://github.com/vuejs/rollup-plugin-vue/issues/322 on why using this:
 import typescript from 'rollup-plugin-typescript2'
+import postcss from 'rollup-plugin-postcss'
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 
 // eslint-disable-next-line import/no-anonymous-default-export -- Used internally by Rollup
-export default {
+export default [{
     input: 'src/components/index.ts',
     output: {
         format: 'esm',
@@ -20,11 +18,22 @@ export default {
         // Vue plugin won't handle CSS currently
         postcss(),
         typescript()
-        // with @rollup/plugin-typescript
-        /*
-        typescript({
-          include: [/\.tsx?$/, /\.vue\?.*?lang=ts/]
-        }),
-        */
     ]
-}
+}, {
+    input: 'src/components/index.ts',
+    output: {
+        format: 'esm',
+        file: 'dist/bundle-browser.js'
+    },
+    plugins: [
+        resolve(),
+        vue(),
+        // Vue plugin won't handle CSS currently
+        postcss(),
+        typescript(),
+        // Not defined in browser
+        replace({
+          'process.env.NODE_ENV': "'production'"
+        }),
+    ]
+}]

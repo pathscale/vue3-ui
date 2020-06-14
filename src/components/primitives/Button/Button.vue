23cef310a122443f77494e505ae76532401b7351
David Cedres <david.cedresx@gmail.com>
2020-06-14 12:39:28 -0400

    fix lint issue, add comment for bug

--- src/components/primitives/Button/Button.vue
+++ src/components/primitives/Button/Button.vue
@@ -1,5 +1,6 @@
 <template>
-  <button
+  <component
+    :is="computedTag"
     class="button"
     :type="nativeType"
     v-bind="$attrs"
@@ -16,103 +17,96 @@
     }]">
     <span v-if="label">{{ label }}</span>
     <slot />
-  </button>
+  </component>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script>
+import { defineComponent, computed } from 'vue'
 
 export default defineComponent({
-    name: 'Button',
-    props: {
-        type: {
-            type: String,
-            default: 'button'
-        },
-        size: {
-            type: String,
-            default: ''
-        },
-        label: {
-            type: String,
-            default: '',
-        },
-        iconPack: {
-            type: String,
-            default: '',
-        },
-        iconLeft: {
-            type: String,
-            default: '',
-        },
-        iconRight: {
-            type: String,
-            default: '',
-        },
-        rounded: {
-            type: Boolean,
-            default: false,
-        },
-        loading: {
-          type: Boolean,
-          default: false,
-        },
-        outlined: {
-          type: Boolean,
-          default: false,
-        },
-        expanded: {
-          type: Boolean,
-          default: false,
-        },
-        inverted: {
-          type: Boolean,
-          default: false,
-        },
-        focused: {
-          type: Boolean,
-          default: false,
-        },
-        active: {
-          type: Boolean,
-          default: false,
-        },
-        hovered: {
-          type: Boolean,
-          default: false,
-        },
-        selected: {
-          type: Boolean,
-          default: false,
-        },
-        nativeType: {
+  // extrange behaviour for value 'Button', conflicts with :is on line 3, recursive loop?
+  name: 'ButtonComponent',
+  props: {
+      type: {
           type: String,
-          default: 'button',
-          validator(value) {
-              return [
-                  'button',
-                  'submit',
-                  'reset'
-              ].includes(value)
-          }
-        },
-        tag: {
-            type: String,
-            default: 'button',
-            validator(value) {
-                return [
-                  'a',
-                  'button',
-                  'input',
-                  'router-link',
-                  'nuxt-link',
-                  'n-link',
-                  'RouterLink',
-                  'NuxtLink',
-                  'NLink'
-              ].includes(value)
-            }
+          default: 'button'
+      },
+      size: {
+          type: String,
+          default: null
+      },
+      label: {
+          type: String,
+          default: null,
+      },
+      rounded: {
+          type: Boolean,
+          default: false,
+      },
+      loading: {
+        type: Boolean,
+        default: false,
+      },
+      outlined: {
+        type: Boolean,
+        default: false,
+      },
+      expanded: {
+        type: Boolean,
+        default: false,
+      },
+      inverted: {
+        type: Boolean,
+        default: false,
+      },
+      focused: {
+        type: Boolean,
+        default: false,
+      },
+      active: {
+        type: Boolean,
+        default: false,
+      },
+      hovered: {
+        type: Boolean,
+        default: false,
+      },
+      selected: {
+        type: Boolean,
+        default: false,
+      },
+      nativeType: {
+        type: String,
+        default: 'button',
+        validator(value) {
+            return [
+                'button',
+                'submit',
+                'reset'
+            ].includes(value)
         }
-    }
+      },
+      tag: {
+        type: String,
+        default: 'button',
+        validator(value) {
+          return [
+            'a',
+            'button',
+            'input',
+            'router-link',
+            'nuxt-link',
+            'n-link',
+            'RouterLink',
+            'NuxtLink',
+            'NLink'
+          ].includes(value)
+        }
+      }
+  },
+  setup(props, { attrs }) {
+    const computedTag = computed(() => attrs.disabled ? 'button' : props.tag)
+    return { computedTag }
+  }
 })
 </script>

1d68420cd005e912be3c607b2939964ba12cf492
blacksonic <soos.gabor86@gmail.com>
2020-06-16 21:08:16 +0200

    - Add three column layout

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+export { default as ColumnsLayout } from "./layouts/Columns/Columns.vue";
 export { default as LoginLayout } from "./layouts/Login/Login.vue";
 
 export { default as LoginFormPanel } from "./panels/LoginForm/LoginForm.vue";
---
+++ src/views/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <ColumnsLayout>
+    <template v-slot:navbar-left>
+      <a class="navbar-item">Home</a>
+      <a class="navbar-item">Documentation</a>
+    </template>
+    <template v-slot:navbar-right>
+      <a class="button is-primary">Sign up</a>
+      <a class="button is-light">Log in</a>
+    </template>
+    <template v-slot:main>
+      <LoginFormPanel />
+    </template>
+  </ColumnsLayout>
+</template>
+
+<script>
+  import { ColumnsLayout, LoginFormPanel } from "../components"
+
+  export default {
+    components: { ColumnsLayout, LoginFormPanel }
+  }
+</script>

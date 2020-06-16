cae5484bdd620b0a834707bbf863848fb1fa1116
David Cedres <david.cedresx@gmail.com>
2020-06-16 07:56:08 -0400

    add container component with bg support

---
+++ src/components/primitives/Container/Container.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="container"
+    :class="[type]"
+    :style="[ bg ? {
+      'background-image': `url(${bg})`,
+      'background-size': 'cover',
+      'background-repeat': 'no-repeat',
+    } : {}]">
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'VContainer',
+    props: {
+        type: {
+            type: String,
+            default: '',
+            validator(value) {
+                return ['is-fluid', 'is-widescreen', 'is-fullhd'].includes(value);
+            }
+        },
+        bg: {
+            type: Object,
+            default: null,
+        }
+    }
+})
+</script>
--- src/views/Showcase.vue
+++ src/views/Showcase.vue
@@ -1,69 +1,73 @@
 <template>
-  <h1 class="title is-1">
-    Checkbox
-  </h1>
-  <Checkbox v-model="toggle">
-    Toggle me
-  </Checkbox>
-  <h1 class="title is-1">
-    Button
-  </h1>
-  <Button type="is-primary">
-    Button
-  </Button>
-  <Button type="is-success">
-    Button
-  </Button>
-  <Button type="is-danger">
-    Button
-  </Button>
-  <Button type="is-warning">
-    Button
-  </Button>
-  <Button type="is-info">
-    Button
-  </Button>
-  <Button type="is-link">
-    Button
-  </Button>
-  <Button type="is-dark">
-    Button
-  </Button>
-  <Button type="is-text">
-    Button
-  </Button>
-  <Button type="is-primary" disabled>
-    Button
-  </Button>
-  <Button type="is-success" loading>
-    Button
-  </Button>
-  <Button type="is-danger" rounded>
-    Button
-  </Button>
-  <Button type="is-warning" outlined>
-    Button
-  </Button>
-  <Button type="is-info" expanded>
-    Button
-  </Button>
-  <h1 class="title is-1">
-    Progress
-  </h1>
-  <Progress type="is-success" value="35" />
+  <Container :bg="bg" type="is-fluid">
+    <h1 class="title is-1">
+      Checkbox
+    </h1>
+    <Checkbox v-model="toggle">
+      Toggle me
+    </Checkbox>
+    <h1 class="title is-1">
+      Button
+    </h1>
+    <Button type="is-primary">
+      Button
+    </Button>
+    <Button type="is-success">
+      Button
+    </Button>
+    <Button type="is-danger">
+      Button
+    </Button>
+    <Button type="is-warning">
+      Button
+    </Button>
+    <Button type="is-info">
+      Button
+    </Button>
+    <Button type="is-link">
+      Button
+    </Button>
+    <Button type="is-dark">
+      Button
+    </Button>
+    <Button type="is-text">
+      Button
+    </Button>
+    <Button type="is-primary" disabled>
+      Button
+    </Button>
+    <Button type="is-success" loading>
+      Button
+    </Button>
+    <Button type="is-danger" rounded>
+      Button
+    </Button>
+    <Button type="is-warning" outlined>
+      Button
+    </Button>
+    <Button type="is-info" expanded>
+      Button
+    </Button>
+    <h1 class="title is-1">
+      Progress
+    </h1>
+    <Progress type="is-success" value="35" />
+  </Container>
 </template>
 
 <script>
 import { ref } from 'vue';
 import Button from '../components/primitives/Button/Button.vue';
+import Container from '../components/primitives/Container/Container.vue';
 import Checkbox from '../components/primitives/Checkbox/Checkbox.vue';
 import Progress from '../components/primitives/Progress/Progress.vue';
+import bg from '../assets/bg.svg';
 
 const View = {
-    components: { Button, Checkbox, Progress },
+    components: { Button, Container, Checkbox, Progress },
     setup(props) {
         const toggle = ref(true);
-        return { toggle };
+        return { toggle, bg };
     }
 }
 

eb2df92cebaa08d4df000f4cdc17a90f35a70f7c
Oscar <oalbornoz08@gmail.com>
2020-06-19 10:26:58 -0400

    export primitive components

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,12 @@ export { default as NavLayout } from "./layouts/Nav/Nav.vue";
 
 export { default as LoginFormPanel } from "./panels/LoginForm/LoginForm.vue";
 
-export { default as InputComponent } from "./primitives/Input/Input.vue";
+export { default as Button } from "./primitives/Select/Select.vue";
+export { default as Checkbox } from "./primitives/Checkbox/Checkbox.vue";
+export { default as Collapse } from "./primitives/Collapse/Collapse.vue";
+export { default as Container } from "./primitives/Container/Container.vue";
+export { default as Field } from "./primitives/Field/Field.vue";
+export { default as Input } from "./primitives/Input/Input.vue";
+export { default as Progress } from "./primitives/Progress/Progress.vue";
+export { default as Select } from "./primitives/Select/Select.vue";
+export { default as Switch } from "./primitives/Switch/Switch.vue";

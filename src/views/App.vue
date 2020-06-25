9d8400a29885b234904501541225f719228223e4
felipemuner <felipe.muner@gmail.com>
2020-06-25 11:59:33 +0700

    test textarea component

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,60 +1,65 @@
 <template>
-  <div class="sidebar-page">
-    <section class="hero is-fullheight sidebar-layout">
-      <v-sidebar
-        position="static"
-        :mobile="mobile"
-        :expand-on-hover="expandOnHover"
-        :reduce="reduce"
-        type="is-light"
-        fullheight
-        :open="open">
-        <div class="p-1">
-          <div class="block">
-            <h1>VSidebar</h1>
+  <div>
+    <div>
+      <VTextarea />
+    </div>
+    <div class="sidebar-page">
+      <section class="hero is-fullheight sidebar-layout">      
+        <v-sidebar
+          position="static"
+          :mobile="mobile"
+          :expand-on-hover="expandOnHover"
+          :reduce="reduce"
+          type="is-light"
+          fullheight
+          :open="open">
+          <div class="p-1">
+            <div class="block">
+              <h1>VSidebar</h1>
+            </div>
           </div>
-        </div>
-      </v-sidebar>
+        </v-sidebar>
 
-      <div class="p-1">
-        <v-field>
-          <v-switch v-model="open">
-            Open
-          </v-switch>
-        </v-field>
-        <v-field>
-          <v-switch v-model="reduce">
-            Reduced
-          </v-switch>
-        </v-field>
-        <v-field>
-          <v-switch v-model="expandOnHover">
-            Expand on hover
-          </v-switch>
-        </v-field>
-        <v-field label="Mobile Layout">
-          <v-select v-model="mobile">
-            <option value="reduce">
+        <div class="p-1">
+          <v-field>
+            <v-switch v-model="open">
+              Open
+            </v-switch>
+          </v-field>
+          <v-field>
+            <v-switch v-model="reduce">
               Reduced
-            </option>
-            <option value="hide">
-              Hidden
-            </option>
-            <option value="fullwidth">
-              Fullwidth
-            </option>
-          </v-select>
-        </v-field>
-      </div>
-    </section>
+            </v-switch>
+          </v-field>
+          <v-field>
+            <v-switch v-model="expandOnHover">
+              Expand on hover
+            </v-switch>
+          </v-field>
+          <v-field label="Mobile Layout">
+            <v-select v-model="mobile">
+              <option value="reduce">
+                Reduced
+              </option>
+              <option value="hide">
+                Hidden
+              </option>
+              <option value="fullwidth">
+                Fullwidth
+              </option>
+            </v-select>
+          </v-field>
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
-import { VSidebar, VSelect, VField, VSwitch } from "../components"
+import { VTextarea, VInput, VSidebar, VSelect, VField, VSwitch } from "../components"
 
 const Component = {
-  components: { VSidebar, VSelect, VField, VSwitch },
+  components: { VTextarea, VInput, VSidebar, VSelect, VField, VSwitch },
   data() {
     return {
       open: true,

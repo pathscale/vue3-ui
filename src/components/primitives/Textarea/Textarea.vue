db96a3b67913cf7422f50cbd1bfaca32e5e23687
David Cedres <david.cedresx@gmail.com>
2020-06-27 05:35:52 -0400

    Fix VTextarea

--- src/components/primitives/Textarea/Textarea.vue
+++ src/components/primitives/Textarea/Textarea.vue
@@ -1,60 +1,42 @@
 <script>
-import { ref, watchEffect, computed } from 'vue'
+    import { ref, watchEffect, computed } from 'vue'
 
-const Textarea = {
-    name: 'VTextarea',
-    inheritAttrs: false,
-    emits:['update:modelValue'],
-    props: {
-        value: [Number, String],
-        hasCounter: {
-            type: Boolean,
+    const Textarea = {
+        name: 'VTextarea',
+        inheritAttrs: false,
+        emits:['update:modelValue'],
+        props: {
+            color: { type: String, default: '' },
+            hasCounter: { type: Boolean, default: false },
+            loading: { type: Boolean, default: false },
+            maxlength: { type: String, default: '' },
+            modelValue: { type: String, default: '' },
+            size: { type: String, default: '' },
         },
-        customClass: {
-            type: String,
-            default: ''
+        setup(props, { emit }) {
+            const value = ref(props.modelValue);
+            const isFocused = ref(false);
+            const onFocus = () => { isFocused.value = true };
+            const onBlur = () => { isFocused.value = false };
+            const valueLength = computed(() => value.value.length);
+            const rootClasses = computed(() => {
+                return [
+                    props.size,
+                    {
+                        'is-loading': props.loading,
+                    }
+                ]
+            });
+            const inputClasses = computed(() => {
+                return [props.size, props.color]
+            });
+
+            watchEffect(() => emit('update:modelValue', value.value));
+            return { value, isFocused, onBlur, onFocus, valueLength, rootClasses, inputClasses };
         },
-        size: String,
-        maxlength: Number,
+    }
 
-        expanded: Boolean,
-        loading: Boolean,
-        rounded: Boolean,
-    },
-    setup(props, { emit, attrs }) {
-        console.log(props)
-        console.log(attrs)
-        const value = ref(attrs.modelValue)
-        watchEffect(() => emit('update:modelValue', value.value))
-        
-        const valueLength = computed(() => value.value.length)
-        watchEffect(() => {
-            console.log(valueLength)
-        })
-        const rootClasses = computed(() => {
-            return [
-                props.size,
-                {
-                    'is-expanded': props.expanded,
-                    'is-loading': props.loading,
-                }
-            ]
-        });
-
-        const inputClasses = computed(() => {
-            return [
-                props.statusType,
-                props.size,
-                { 'is-rounded': props.rounded }
-            ]
-        })
-
-
-        return { value, rootClasses, inputClasses, valueLength }
-    },
-}
-
-export default Textarea;
+    export default Textarea;
 </script>
 
 <template>
@@ -65,9 +47,11 @@ export default Textarea;
       ref="textarea"
       class="textarea"
       v-model="value"
-      :class="[inputClasses, customClass]"
-      :maxlength="maxlength"      
-      v-bind="$attrs" />
+      :class="inputClasses"
+      :maxlength="maxlength"
+      v-bind="$attrs"
+      @blur="onBlur"
+      @focus="onFocus" />
     <small
       v-if="maxlength && hasCounter"
       class="help counter"

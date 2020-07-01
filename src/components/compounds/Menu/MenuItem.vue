06ce3fd1c81b115fb447a9dd8abdb77376d5d699
Oscar <oalbornoz08@gmail.com>
2020-07-01 16:10:08 -0400

    test icon prop in menu item

--- src/components/compounds/Menu/MenuItem.vue
+++ src/components/compounds/Menu/MenuItem.vue
@@ -64,6 +64,7 @@ export default Component
         'is-disabled': newExpanded
       }"
       @click="onClick($event)">
+      <span v-if="icon"> {{ icon }} </span>
       <span v-if="label">
         {{ label }}</span>
       <slot

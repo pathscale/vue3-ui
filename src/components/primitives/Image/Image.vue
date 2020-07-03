980dd2838e3b25cb016b3cf79cdac8b6861a8495
Oscar <oalbornoz08@gmail.com>
2020-07-03 18:10:08 -0400

    Add Image component

---
+++ src/components/primitives/Image/Image.vue
@@ -0,0 +1,31 @@
+<script>
+
+const Button = {
+  name: 'VImage',
+  inheritAttrs: false,
+  props: {
+      size: {
+          type: String,
+          default: null
+      },
+      radio: {
+          type: String,
+          default: null,
+      },
+      rounded: {
+        type: Boolean,
+      },
+  }
+}
+
+export default Button;
+</script>
+
+<template>
+  <img
+    class="image"
+    v-bind="$attrs"
+    :class="[size, radio, {
+      'is-rounded': rounded
+    }]" />
+</template>

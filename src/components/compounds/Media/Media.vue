a8fcbed62f90dd1f4365feeff7ff7e62577076f0
Oscar <oalbornoz08@gmail.com>
2020-07-08 14:29:22 -0400

    Rename Message component and use bulma classes

---
+++ src/components/compounds/Media/Media.vue
@@ -0,0 +1,21 @@
+<script>
+const Media = {
+  name: "VMedia",
+  props: {},
+}
+export default Media
+</script>
+
+<template>
+  <article class="media">
+    <div class="media-left">
+      <slot name="left">
+    </div>
+    <div class="media-content">
+      <slot name="content">
+    </div>
+    <div class="media-right">
+      <slot name="right">
+    </div>
+  </article>
+</template>

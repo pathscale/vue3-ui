30f45fcbdcc5785d52a402e70b51529327baedf8
Brett Zamir <brettz9@yahoo.com>
2020-10-06 13:54:35 +0800

    - Linting: Better modularize linting; override lack of consistency in prettier with semicolons

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,65 @@
 'use strict'
 
+const baseConfigs = [
+  'ash-nazg/sauron',
+  'plugin:vue/vue3-strongly-recommended',
+  'plugin:import/errors',
+  'plugin:import/warnings',
+  'plugin:prettier/recommended',
+]
+
+const baseTSConfigs = [
+  ...baseConfigs,
+  'plugin:import/typescript',
+  'plugin:@typescript-eslint/recommended',
+]
+
+const baseRules = {
+  'no-extra-semi': ['error'],
+  // semi: ['error', 'never'],
+  'no-restricted-syntax': [
+    'error',
+    {
+      selector: 'ImportNamespaceSpecifier',
+      message:
+        'As it is wasteful to use all named imports, use only those ' +
+        'which are needed (or switch to a default import).',
+    },
+  ],
+
+  // Disabling for now
+  'import/extensions': 'off',
+  'jsdoc/require-jsdoc': 'off',
+  'require-unicode-regexp': 'off',
+  'prefer-named-capture-group': 'off',
+
+  // Keep this here so can uncomment to check inline disabling
+  // "eslint-comments/no-use": "error",
+
+  'vue/max-attributes-per-line': 'off',
+  'vue/html-closing-bracket-newline': [
+    'error',
+    {
+      singleline: 'never',
+      multiline: 'never',
+    },
+  ],
+  'vue/html-self-closing': [
+    'error',
+    {
+      html: {
+        void: 'always',
+      },
+    },
+  ],
+}
+
 module.exports = {
   env: {
     browser: true,
     es6: true,
   },
-  extends: [
-    'ash-nazg/sauron',
-    'plugin:vue/vue3-strongly-recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:prettier/recommended',
-  ],
+  extends: baseConfigs,
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -26,13 +74,7 @@ module.exports = {
         '.3rdparty-eslintrc.js',
         '.np-config.js',
       ],
-      extends: [
-        'ash-nazg/sauron-node',
-        'plugin:vue/vue3-strongly-recommended',
-        'plugin:import/errors',
-        'plugin:import/warnings',
-        'plugin:prettier/recommended',
-      ],
+      extends: baseConfigs,
       env: {
         node: true,
       },
@@ -47,20 +89,18 @@ module.exports = {
         sourceType: 'script',
       },
       rules: {
+        ...baseRules,
         strict: ['error', 'global'],
         'import/no-commonjs': 'off',
-
-        // Disabling for now
-        'require-unicode-regexp': 'off',
-        'prefer-named-capture-group': 'off',
       },
     },
     {
       files: '*.ts',
-      extends: ['plugin:import/typescript', 'plugin:@typescript-eslint/recommended'],
+      extends: baseTSConfigs,
       plugins: ['@typescript-eslint'],
       parser: '@typescript-eslint/parser',
       rules: {
+        ...baseRules,
         // Reenable later
         'eslint-comments/require-description': 0,
         'eslint-comments/no-unused-disable': 0,
@@ -75,53 +115,22 @@ module.exports = {
     },
     {
       files: 'src/shims-vue.d.ts',
+      extends: baseTSConfigs,
       rules: {
+        ...baseRules,
         // No imports/exports in plain declaration file
         'import/unambiguous': 'off',
       },
     },
     {
       files: '*.vue',
+      extends: baseConfigs,
       rules: {
-        'prettier/prettier': 'off',
+        ...baseRules,
       },
     },
   ],
   rules: {
-    'no-restricted-syntax': [
-      'error',
-      {
-        selector: 'ImportNamespaceSpecifier',
-        message:
-          'As it is wasteful to use all named imports, use only those ' +
-          'which are needed (or switch to a default import).',
-      },
-    ],
-
-    // Disabling for now
-    'import/extensions': 'off',
-    'jsdoc/require-jsdoc': 'off',
-    'require-unicode-regexp': 'off',
-    'prefer-named-capture-group': 'off',
-
-    // Keep this here so can uncomment to check inline disabling
-    // "eslint-comments/no-use": "error",
-
-    'vue/max-attributes-per-line': 'off',
-    'vue/html-closing-bracket-newline': [
-      'error',
-      {
-        singleline: 'never',
-        multiline: 'never',
-      },
-    ],
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'always',
-        },
-      },
-    ],
+    ...baseRules,
   },
 }

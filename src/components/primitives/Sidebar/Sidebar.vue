638fb3927211de18094d9753357101874329898c
David <david.cedresx@gmail.com>
2020-11-10 08:30:02 -0400

    add separation of concerns to sidebar component

--- src/components/primitives/Sidebar/Sidebar.vue
+++ src/components/primitives/Sidebar/Sidebar.vue
@@ -24,36 +24,24 @@ export default {
     },
     width: {
       type: [Number, String],
-      default: 260
+      default: 260,
     },
     miniWidth: {
       type: [Number, String],
-      default: 80
-    }
+      default: 80,
+    },
   },
   emits: ['close'],
   setup(props, { emit }) {
     const state = reactive({
       transitionName: null,
-      animating: true
+      animating: true,
     })
 
-    const rootClasses = computed(() => {
-      return [props.type, {
-        'is-fixed': isFixed.value,
-        'is-static': isStatic.value,
-        'is-absolute': isAbsolute.value,
-        'is-fullheight': props.fullheight,
-        'is-fullwidth': props.fullwidth,
-        'is-right': props.right,
-        'is-mini': props.reduce,
-        'is-mini-expand': props.expandOnHover,
-        'is-mini-expand-fixed': props.expandOnHover && props.expandOnHoverFixed,
-        'is-mini-mobile': props.mobile === 'reduce',
-        'is-hidden-mobile': props.mobile === 'hide',
-        'is-fullwidth-mobile': props.mobile === 'fullwidth'
-      }]
-    })
+    const isMiniExpandFixed = computed(() => props.expandOnHover && props.expandOnHoverFixed)
+    const isMiniMobile = computed(() => props.mobile === 'reduce')
+    const isHiddenMobile = computed(() => props.mobile === 'hide')
+    const isFullwidthMobile = computed(() => props.mobile === 'fullwidth')
 
     const rootStyles = computed(() => {
       return `width: ${props.reduce ? props.miniWidth : props.width}px`
@@ -84,24 +72,49 @@ export default {
       state.transitionName = open ? 'slide-right' : 'slide-left'
     })
 
-    return { state, rootClasses, rootStyles, cancelOptions, emit, overlayAndOpen }
+    return {
+      state,
+      rootStyles,
+      cancelOptions,
+      emit,
+      overlayAndOpen,
+      isMiniExpandFixed,
+      isMiniMobile,
+      isHiddenMobile,
+      isFullwidthMobile,
+      isStatic,
+      isFixed,
+      isAbsolute,
+    }
   },
 }
 </script>
 
 <template>
   <div class="v-sidebar">
-    <div
-      class="sidebar-background"
-      v-if="overlayAndOpen"
-      @click="emit('close')" />
-    <transition
-      :name="state.transitionName">
+    <div class="sidebar-background" v-if="overlayAndOpen" @click="emit('close')" />
+    <transition :name="state.transitionName">
       <div
         v-if="open"
         ref="sidebarContent"
         class="sidebar-content is-width-animated"
-        :class="rootClasses"
+        :class="[
+          props.type,
+          {
+            'is-fixed': isFixed.value,
+            'is-static': isStatic.value,
+            'is-absolute': isAbsolute.value,
+            'is-fullheight': props.fullheight,
+            'is-fullwidth': props.fullwidth,
+            'is-right': props.right,
+            'is-mini': props.reduce,
+            'is-mini-expand': props.expandOnHover,
+            'is-mini-expand-fixed': isMiniExpandFixed,
+            'is-mini-mobile': isMiniMobile,
+            'is-hidden-mobile': isHiddenMobile,
+            'is-fullwidth-mobile': isFullwidthMobile,
+          },
+        ]"
         :style="rootStyles">
         <slot />
       </div>

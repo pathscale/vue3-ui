9e4e9d0d5d30e26b17af29fa331739e0170d7bbe
munirapp <softwaremakassar@gmail.com>
2020-11-21 01:48:31 +0800

    Add avatar component

--- src/components/primitives/Avatar/Avatar.vue
+++ src/components/primitives/Avatar/Avatar.vue
@@ -34,8 +34,6 @@ export default {
     },
     setup(props){
         const source = ref(props.src || props.dataSrc)
-        const background = ref(props.background)
-        const text = ref(props.text)
         const alt = ref(props.alt)
 
         onBeforeMount(async() => {
@@ -48,9 +46,9 @@ export default {
             }
         })
 
-        const backgroundColor = source.value ? `has-background-${background.value}` : ''
+        const backgroundColor = source.value ? `has-background-${props.background}` : ''
 
-        const textColor = source.value ? `has-text-${text.value}` : ''
+        const textColor = source.value ? `has-text-${props.text}` : ''
 
         const caption = alt.value.split(" ")[0][0] + alt.value.split(" ")[1][0]
 

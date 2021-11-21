6ceda19e04afe71c82255c3d95954ef1fefbf33f
Oscar Albornoz <oalbornoz08@gmail.com>
2021-11-21 14:23:31 -0400

    Add calendar component

--- src/components/compounds/Toast/Toaster.js
+++ src/components/compounds/Toast/Toaster.js
@@ -2,13 +2,11 @@ import Toaster from './Toaster.vue'
 import createToaster from './api.js'
 import ToasterPositions from './defaults/positions.js'
 
-const Plugin = (app, options = {}) => {
+Toaster.install = (app, options = {}) => {
   const methods = createToaster(options)
   app.config.globalProperties.$toast = methods
-  app.provide('toast', methods)
+  app.provide('$toast', methods)
 }
-
-Toaster.install = Plugin
 
 export default Toaster
 export { Toaster, ToasterPositions, createToaster }

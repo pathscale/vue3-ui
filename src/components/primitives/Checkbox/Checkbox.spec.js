36ec8eded194bc18b198f07e89b926d17feae404
Oscar Albornoz <oalbornoz08@gmail.com>
2021-12-01 10:50:04 -0400

    Add checkbox tests

--- src/components/primitives/Checkbox/Checkbox.spec.js
+++ src/components/primitives/Checkbox/Checkbox.spec.js
@@ -15,4 +15,27 @@ describe('Checkbox', () => {
   it('render correctly', () => {
     expect(wrapper.html()).toMatchSnapshot()
   })
+
+  it('has an input checkbox', () => {
+    expect(wrapper.html()).toContain('input type="checkbox"')
+  })
+
+  it('emit input event with value when value change', async () => {
+    wrapper.setProps({ modelValue: true })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.value).toBeTruthy()
+
+    wrapper.vm.value = false
+    await wrapper.vm.$nextTick()
+
+    const valueEmitted = wrapper.emitted()['update:modelValue'][2]
+    expect(valueEmitted).toContainEqual(false)
+  })
+
+  it('method focus() gives focus to the input element', async () => {
+    wrapper.vm.$refs.input.focus = jest.fn()
+    wrapper.vm.focus()
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.$refs.input.focus).toHaveBeenCalledTimes(1)
+  })
 })

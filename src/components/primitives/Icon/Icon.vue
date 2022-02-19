4cc9eee9d5c44c2ae4300b18a4f9aaf0f1f1e0c4
Oscar Albornoz <oalbornoz08@gmail.com>
2022-02-19 14:36:09 -0400

    feat: add cacheVersion prop to Icon component

--- src/components/primitives/Icon/Icon.vue
+++ src/components/primitives/Icon/Icon.vue
@@ -12,22 +12,26 @@ export default {
     customSize: String,
     fill: {
       type: String,
-    }
+    },
+    cacheVersion: {
+      type: String,
+      default: '1.0.0',
+    },
   },
   setup(props) {
     const shouldIconClass = computed(() => !props.customSize && !props.customClass)
     // Only apply 100% size when bulma icon class is used
-    const computedHeight = computed(() => !shouldIconClass.value ? props.customSize : "100%")
-    const computedWidth = computed(() => !shouldIconClass.value ? props.customSize : "100%")
+    const computedHeight = computed(() => (!shouldIconClass.value ? props.customSize : '100%'))
+    const computedWidth = computed(() => (!shouldIconClass.value ? props.customSize : '100%'))
     return { computedHeight, computedWidth, shouldIconClass }
   },
 }
 </script>
 
 <template>
-  <span :class="[{'icon': shouldIconClass}, size]">
+  <span :class="[{ 'icon': shouldIconClass }, size]">
     <svg :fill="fill" :height="computedHeight" :width="computedWidth" :class="[customClass]">
-      <use :href="`/${bundle}.svg#${name}`" />
+      <use :href="`/${bundle}.svg?v=${cacheVersion}#${name}`" />
     </svg>
   </span>
 </template>

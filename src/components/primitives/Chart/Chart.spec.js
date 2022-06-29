a5dfbe41fa6673acd6a84b3a2157423c15470074
Skaptox <oalbornoz08@gmail.com>
2022-06-28 21:01:55 -0400

    feat: add chart component (#5)
    * feat/chart
    
    * feat/chart
    * mappings
    
    Co-authored-by: blue32captain <blue32captain@gmail.com>

---
+++ src/components/primitives/Chart/Chart.spec.js
@@ -0,0 +1,13 @@
+import Chart from './Chart.vue'
+
+let wrapper
+
+describe('Chart', () => {
+  beforeEach(() => {
+    wrapper = Chart
+  })
+
+  it('initialize', () => {
+    expect(wrapper.exist).toBe(wrapper.exist)
+  })
+})

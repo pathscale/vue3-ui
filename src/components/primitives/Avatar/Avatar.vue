4c9375b4e61fbc6d004963b7546f61f0d77166cd
Lukas <kosha.industry@gmail.com>
2025-04-17 22:54:54 +0500

    rewrite to vue3

--- src/components/primitives/Avatar/Avatar.vue
+++ src/components/primitives/Avatar/Avatar.vue
@@ -14,64 +14,62 @@
   </figure>
 </template>
 
-<script>
+<script setup lang="ts">
 /* eslint-disable no-undef -- Access to Benchie variable/function */
-import { onBeforeMount, ref } from 'vue'
-
-export default {
-  name: 'VAvatar',
-  props: {
-    alt: String,
-    size: {
-      type: String,
-      default: 'is-64x64',
-    },
-    rounded: {
-      type: Boolean,
-      default: true,
-    },
-    background: {
-      type: String,
-      default: 'has-background-link',
-    },
-    text: {
-      type: String,
-      default: 'has-text-white',
-    },
-    src: {
-      type: String,
-      default: null,
-    },
-    dataSrc: {
-      type: String,
-      default: null,
-    },
-    customClass: String,
-  },
-  setup(props) {
-    const source = ref(props.src || props.dataSrc)
-    const background = ref(props.background)
-    const text = ref(props.text)
-    const alt = ref(props.alt)
-
-    // todo fix: same as image.vue
-    const hasBenchieSupport = false
-
-    onBeforeMount(async () => {
-      if (props.dataSrc && hasBenchieSupport) {
-        source.value = await t(props.dataSrc, $__CDN)
-      }
-    })
-
-    const backgroundColor = source.value ? '' : background.value
-
-    const textColor = source.value ? '' : text.value
-
-    const caption = alt.value ? alt.value.split(' ')[0][0] + alt.value.split(' ')[1][0] : ""
-
-    return { source, backgroundColor, textColor, caption }
-  },
-}
+// import { onBeforeMount, ref } from 'vue'
+//
+// export default {
+//   name: 'VAvatar',
+//   props: {
+//     alt: String,
+//     size: {
+//       type: String,
+//       default: 'is-64x64',
+//     },
+//     rounded: {
+//       type: Boolean,
+//       default: true,
+//     },
+//     background: {
+//       type: String,
+//       default: 'has-background-link',
+//     },
+//     text: {
+//       type: String,
+//       default: 'has-text-white',
+//     },
+//     src: {
+//       type: String,
+//       default: null,
+//     },
+//     dataSrc: {
+//       type: String,
+//       default: null,
+//     },
+//     customClass: String,
+//   },
+//   setup(props) {
+//     const source = ref(props.src || props.dataSrc)
+//     const background = ref(props.background)
+//     const text = ref(props.text)
+//     const alt = ref(props.alt)
+//
+//     // todo fix: same as image.vue
+//     const hasBenchieSupport = false
+//
+//     onBeforeMount(async () => {
+//       if (props.dataSrc && hasBenchieSupport) {
+//         source.value = await t(props.dataSrc, $__CDN)
+//       }
+//     })
+//
+//     const backgroundColor = source.value ? '' : background.value
+//
+//     const textColor = source.value ? '' : text.value
+//
+//     const caption = alt.value ? alt.value.split(' ')[0][0] + alt.value.split(' ')[1][0] : ""
+//
+//     return { source, backgroundColor, textColor, caption }
+//   },
+// }
 </script>
-
-

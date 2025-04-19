7613791ac929144fb52f001d0cf0fcea8b8f6405
Lukas <kosha.industry@gmail.com>
2025-04-19 16:58:52 +0500

    test: container component

---
+++ src/components/primitives/Container/__tests__/Container.spec.ts
@@ -0,0 +1,10 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+import Container from "../Container.vue";
+
+describe("Container", () => {
+  it("renders with default props", () => {
+    const wrapper = mount(Container);
+    expect(wrapper.classes()).toContain("container");
+  });
+});

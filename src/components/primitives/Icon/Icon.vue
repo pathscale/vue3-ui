e6fed0a21df2c3f68858d1c625a8e87032811c48
Lukas <kosha.industry@gmail.com>
2025-04-19 17:50:24 +0500

    refactor(primitives): modern vue sfc style

--- src/components/primitives/Icon/Icon.vue
+++ src/components/primitives/Icon/Icon.vue
@@ -1,37 +1,33 @@
-<script>
+<script setup lang="ts">
 import { computed } from "vue";
 
-export default {
-  name: "VIcon",
+defineOptions({
   inheritAttrs: false,
-  props: {
-    size: String,
-    bundle: String,
-    name: String,
-    customClass: String,
-    customSize: String,
-    fill: {
-      type: String,
-    },
-    cacheVersion: {
-      type: String,
-      default: "1.0.0",
-    },
+});
+
+const props = withDefaults(
+  defineProps<{
+    size?: "is-small" | "is-normal" | "is-medium" | "is-large";
+    bundle: string; // required
+    name: string; // required
+    customClass?: string;
+    customSize?: string;
+    fill?: string;
+    cacheVersion?: string;
+  }>(),
+  {
+    cacheVersion: "1.0.0",
   },
-  setup(props) {
-    const shouldIconClass = computed(
-      () => !props.customSize && !props.customClass,
-    );
-    // Only apply 100% size when bulma icon class is used
-    const computedHeight = computed(() =>
-      !shouldIconClass.value ? props.customSize : "100%",
-    );
-    const computedWidth = computed(() =>
-      !shouldIconClass.value ? props.customSize : "100%",
-    );
-    return { computedHeight, computedWidth, shouldIconClass };
-  },
-};
+);
+
+const shouldIconClass = computed(() => !props.customSize && !props.customClass);
+// Only apply 100% size when bulma icon class is used
+const computedHeight = computed(() =>
+  !shouldIconClass.value ? props.customSize : "100%",
+);
+const computedWidth = computed(() =>
+  !shouldIconClass.value ? props.customSize : "100%",
+);
 </script>
 
 <template>

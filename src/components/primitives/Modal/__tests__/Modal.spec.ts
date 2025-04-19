75533011140f8dbd88f76122bb90ab42812637ea
Lukas <kosha.industry@gmail.com>
2025-04-19 19:17:51 +0500

    test: modal component

---
+++ src/components/primitives/Modal/__tests__/Modal.spec.ts
@@ -0,0 +1,22 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, it } from "vitest";
+import Modal from "../Modal.vue";
+
+describe("Modal", () => {
+  it("updates v-model value", async () => {
+    const wrapper = mount(Modal);
+
+    // by default it active
+    expect(wrapper.find(".modal").classes()).toContain("is-active");
+
+    // click close button
+    await wrapper.find(".modal-close").trigger("click");
+    const modelValueEvent = wrapper.emitted("update:modelValue");
+    expect(modelValueEvent).toHaveLength(1);
+    // @ts-ignore
+    expect(modelValueEvent[0]).toEqual([false]);
+
+    // 'is-active' class removed
+    expect(wrapper.find(".modal").classes()).not.toContain("is-active");
+  });
+});

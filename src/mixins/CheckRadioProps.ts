90c95a32cddb1de7463d0439c20af3a17711c22e
Lukas <kosha.industry@gmail.com>
2025-04-20 02:30:32 +0500

    refactor: use defineModel

--- src/mixins/CheckRadioProps.ts
+++ src/mixins/CheckRadioProps.ts
@@ -1,7 +1,5 @@
 export interface CheckRadioProps {
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-  modelValue: any;
-  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   nativeValue?: any;
   type?: string;
   disabled?: boolean;

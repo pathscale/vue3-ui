5813567e8ffe2af665a8816b73bd4e61169db5e9
Oscar <oalbornoz08@gmail.com>
2025-04-19 01:33:16 -0400

    test: add button test

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: [
+        "node_modules/",
+        "dist/",
+        "**/*.d.ts",
+        "**/*.config.{js,ts}",
+        "**/coverage/**",
+      ],
+    },
+  },
+});

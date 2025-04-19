139170806cb0993defa91fe3dc82f831ceae9388
Lukas <kosha.industry@gmail.com>
2025-04-19 17:08:20 +0500

    test: container component

--- src/components/primitives/Container/__tests__/Container.spec.ts
+++ src/components/primitives/Container/__tests__/Container.spec.ts
@@ -9,4 +9,18 @@ describe("Container", () => {
       '<div class="container"></div>',
     );
   });
+
+  it("render with background image", () => {
+    const wrapper = mount(Container, {
+      props: {
+        bg: "https://example.com/image.jpg",
+      },
+    });
+    const containerEl = wrapper.element;
+    expect(containerEl.style.backgroundImage).toBe(
+      "url(https://example.com/image.jpg)",
+    );
+    expect(containerEl.style.backgroundSize).toBe("cover");
+    expect(containerEl.style.backgroundRepeat).toBe("no-repeat");
+  });
 });

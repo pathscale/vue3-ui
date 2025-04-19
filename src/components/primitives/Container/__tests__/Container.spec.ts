1004d6dce8ffe3e5374f511577ab0415c641bc5e
Lukas <kosha.industry@gmail.com>
2025-04-19 17:12:15 +0500

    test: container component

--- src/components/primitives/Container/__tests__/Container.spec.ts
+++ src/components/primitives/Container/__tests__/Container.spec.ts
@@ -80,6 +80,23 @@ describe("Container", () => {
     expect(wrapper.classes()).toContain("custom-class");
   });
 
+  it("render with type class and class attribute", () => {
+    const wrapper = mount(Container, {
+      props: {
+        type: "type-class-1 type-class-2",
+      },
+      attrs: {
+        class: "custom-class-1 custom-class-2",
+      },
+    });
+    expect(wrapper.classes()).length(5);
+    expect(wrapper.classes()).toContain("container");
+    expect(wrapper.classes()).toContain("type-class-1");
+    expect(wrapper.classes()).toContain("type-class-2");
+    expect(wrapper.classes()).toContain("custom-class-1");
+    expect(wrapper.classes()).toContain("custom-class-2");
+  });
+
   it("render with background image", () => {
     const wrapper = mount(Container, {
       props: {

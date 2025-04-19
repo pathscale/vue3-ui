093e6ca50f1ba80548f626ce8260ef22c6995bc6
Lukas <kosha.industry@gmail.com>
2025-04-19 16:53:03 +0500

    refactor(primitives): modern vue sfc style

--- src/components/primitives/Container/Container.vue
+++ src/components/primitives/Container/Container.vue
@@ -1,27 +1,23 @@
-<script>
+<script setup lang="ts">
+// todo add docs to https://vue3.dev/documentation
 import { computed } from "vue";
 
-export default {
-  name: "VContainer",
-  props: {
-    type: String,
-    bg: Object,
-  },
-  setup(props) {
-    const rootClasses = computed(() => {
-      return [
-        props.bg
-          ? {
-              "background-image": `url(${props.bg})`,
-              "background-size": "cover",
-              "background-repeat": "no-repeat",
-            }
-          : {},
-      ];
-    });
-    return { rootClasses };
-  },
-};
+const props = defineProps<{
+  type?: string;
+  bg?: string;
+}>();
+
+const rootClasses = computed(() => {
+  return [
+    props.bg
+      ? {
+          "background-image": `url(${props.bg})`,
+          "background-size": "cover",
+          "background-repeat": "no-repeat",
+        }
+      : {},
+  ];
+});
 </script>
 
 <template>

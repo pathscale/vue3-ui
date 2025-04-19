8a4c8cc11bc20e5ea3937aa7b19363dd6a004ffc
Lukas <kosha.industry@gmail.com>
2025-04-19 18:09:48 +0500

    docs: add todo about icons

--- src/components/primitives/Icon/Icon.vue
+++ src/components/primitives/Icon/Icon.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+/*
+ TODO: Update docs at https://vue3.dev/documentation/icons
+   - Remove the unused `src` property from the documentation
+   - Use a separate component (e.g., <v-icons>) for registering the icon sprite,
+     since the current approach—using <v-icon> both for sprite declaration and individual usage—
+     can be confusing
+ */
+
 import { computed } from "vue";
 
 defineOptions({

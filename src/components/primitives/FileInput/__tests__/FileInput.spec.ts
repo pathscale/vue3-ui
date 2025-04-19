319b2c4279145a68b66066a2a81a77507dbc063a
Lukas <kosha.industry@gmail.com>
2025-04-19 17:39:52 +0500

    test: v-file-input component

--- src/components/primitives/FileInput/__tests__/FileInput.spec.ts
+++ src/components/primitives/FileInput/__tests__/FileInput.spec.ts
@@ -3,10 +3,11 @@ import { describe, expect, it } from "vitest";
 import FileInput from "../FileInput.vue";
 
 describe("FileInput", () => {
-  it("file with name attr", () => {
+  it("file input with attributes", () => {
     const wrapper = mount(FileInput, {
       attrs: {
         name: "upload-file",
+        accept: "image/png, image/jpeg",
       },
     });
     const file = wrapper.find('input[type="file"]');
@@ -14,5 +15,6 @@ describe("FileInput", () => {
 
     const fileEl = file.element as HTMLInputElement;
     expect(fileEl.name).toBe("upload-file");
+    expect(fileEl.accept).toBe("image/png, image/jpeg");
   });
 });

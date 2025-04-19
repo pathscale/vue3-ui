6168a08d3649b67223311d555fa93b589ab29349
Lukas <kosha.industry@gmail.com>
2025-04-20 02:35:33 +0500

    refactor: add union types

--- src/types/CheckRadioProps.ts
+++ src/types/CheckRadioProps.ts
@@ -1,9 +1,23 @@
+/**
+ * @see https://vue3.dev/documentation/checkbox#api
+ */
 export interface CheckRadioProps {
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   nativeValue?: any;
-  type?: string;
+  type?:
+    | "is-white"
+    | "is-light"
+    | "is-dark"
+    | "is-black"
+    | "is-text"
+    | "is-primary"
+    | "is-link"
+    | "is-info"
+    | "is-success"
+    | "is-warning"
+    | "is-danger";
   disabled?: boolean;
   required?: boolean;
   name?: string;
-  size?: string;
+  size?: "is-small" | "is-medium" | "is-large";
 }

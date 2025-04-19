530e66a59b94478df564a6b0f3475020096b8b08
Lukas <kosha.industry@gmail.com>
2025-04-19 22:15:29 +0500

    refactor(primitives): modern vue sfc style

--- src/components/primitives/Textarea/Textarea.vue
+++ src/components/primitives/Textarea/Textarea.vue
@@ -1,38 +1,37 @@
-<script>
+<script setup lang="ts">
 import { computed, ref, watchEffect } from "vue";
 
-export default {
-  name: "VTextarea",
+defineOptions({
   inheritAttrs: false,
-  props: {
-    color: String,
-    hasCounter: Boolean,
-    loading: Boolean,
-    maxlength: String,
-    modelValue: String,
-    size: String,
-    hovered: Boolean,
-    focused: Boolean,
-  },
-  emits: ["update:modelValue"],
-  setup(props, { emit }) {
-    const value = ref(props.modelValue);
-    const isFocused = ref(false);
-    const onFocus = () => {
-      isFocused.value = true;
-    };
-    const onBlur = () => {
-      isFocused.value = false;
-    };
-    const valueLength = computed(() => value.value.length);
-    const showCounter = computed(() => props.maxlength && props.hasCounter);
-    watchEffect(() => emit("update:modelValue", value.value));
-    watchEffect(() => {
-      value.value = props.modelValue;
-    });
-    return { value, isFocused, onBlur, onFocus, valueLength, showCounter };
-  },
+});
+
+const props = defineProps<{
+  modelValue?: string;
+  color?: string;
+  hasCounter?: boolean;
+  loading?: boolean;
+  maxlength?: string; // todo | number
+  size?: string; // todo union
+  hovered?: boolean;
+  focused?: boolean;
+}>();
+
+const emit = defineEmits(["update:modelValue"]);
+
+const value = ref(props.modelValue);
+const isFocused = ref(false);
+const onFocus = () => {
+  isFocused.value = true;
 };
+const onBlur = () => {
+  isFocused.value = false;
+};
+const valueLength = computed(() => value.value.length);
+const showCounter = computed(() => props.maxlength && props.hasCounter);
+watchEffect(() => emit("update:modelValue", value.value));
+watchEffect(() => {
+  value.value = props.modelValue;
+});
 </script>
 
 <template>

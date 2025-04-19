61b7a19366bc3aff843544b3790bcfe921d3119b
Lukas <kosha.industry@gmail.com>
2025-04-19 17:14:29 +0500

    test: container component

--- src/components/primitives/Container/__tests__/Container.spec.ts
+++ src/components/primitives/Container/__tests__/Container.spec.ts
@@ -20,6 +20,11 @@ describe("Container", () => {
     expect(wrapper.classes()).toContain("container");
     expect(wrapper.classes()).toContain("type-class-1");
     expect(wrapper.classes()).toContain("type-class-2");
+
+    // check order
+    expect(wrapper.html({ raw: true })).toContain(
+      '<div class="container type-class-1 type-class-2"></div>',
+    );
   });
 
   it("render with class attribute", () => {
@@ -31,6 +36,11 @@ describe("Container", () => {
     expect(wrapper.classes()).length(2);
     expect(wrapper.classes()).toContain("container");
     expect(wrapper.classes()).toContain("custom-class");
+
+    // check order
+    expect(wrapper.html({ raw: true })).toContain(
+      '<div class="container custom-class"></div>',
+    );
   });
 
   it("render with type class and class attribute", () => {
@@ -48,6 +58,11 @@ describe("Container", () => {
     expect(wrapper.classes()).toContain("type-class-2");
     expect(wrapper.classes()).toContain("custom-class-1");
     expect(wrapper.classes()).toContain("custom-class-2");
+
+    // check order
+    expect(wrapper.html({ raw: true })).toContain(
+      '<div class="container type-class-1 type-class-2 custom-class-1 custom-class-2"></div>',
+    );
   });
 
   it("render with background image", () => {

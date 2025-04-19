199078998d4582ce2cab33332405bf8695b24549
Lukas <kosha.industry@gmail.com>
2025-04-20 01:41:34 +0500

    test: upload component

--- src/components/primitives/Upload/__tests__/Upload.spec.ts
+++ src/components/primitives/Upload/__tests__/Upload.spec.ts
@@ -8,4 +8,29 @@ describe("Upload", () => {
     expect(wrapper.find(".file").exists()).toBe(true);
     expect(wrapper.find('input[type="file"]').exists()).toBe(true);
   });
+
+  it("handles file upload", async () => {
+    const wrapper = mount(Upload);
+    const input = wrapper.find('input[type="file"]');
+
+    // emulate file selection
+    const file = new File(["hello"], "hello.txt", { type: "text/plain" });
+
+    Object.defineProperty(input.element, "files", {
+      value: [file],
+      writable: false,
+    });
+
+    input.element.dispatchEvent(new Event("change"));
+
+    // check event
+    const emitted = wrapper.emitted("update:modelValue");
+    expect(emitted).toBeTruthy();
+    expect(emitted).length(1);
+
+    // check emitted file
+    expect(emitted?.[0]).toBeTruthy();
+    const emittedFile = emitted?.[0][0];
+    expect(emittedFile).instanceOf(File);
+  });
 });

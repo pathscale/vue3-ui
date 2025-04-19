c9dd0e7cc66137d093ee4533df5cbc149284af6b
Lukas <kosha.industry@gmail.com>
2025-04-19 19:56:34 +0500

    refactor(primitives): modern vue sfc style

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,4 +1,4 @@
-function checkBenchieSupport() {
+export function checkBenchieSupport() {
   const isProduction = process.env.NODE_ENV === "production";
   if (!isProduction) return false;
   return (
@@ -8,4 +8,34 @@ function checkBenchieSupport() {
     typeof $__CDN === "string"
   );
 }
-export { checkBenchieSupport };
+
+/**
+ * Formats a number with a specified decimal precision.
+ *
+ * Internally shifts the decimal point to perform rounding with correct precision,
+ * then optionally removes trailing zeroes if `props.keepTrailingZeroes` is `false`.
+ *
+ * @param {number|string} num - The input number to format. Can be a number or a numeric string.
+ * @returns {string} The formatted number as a string.
+ *
+ * @example
+ * // props = { precision: 2, keepTrailingZeroes: true }
+ * toFixed(1.2345) // "1.23"
+ *
+ * // props = { precision: 2, keepTrailingZeroes: false }
+ * toFixed(1.2000) // "1.2"
+ * toFixed(1.0000) // "1"
+ */
+export function toFixed(
+  num: number | string,
+  precision = 2,
+  keepTrailingZeroes = false,
+): string {
+  let fixed = Number(
+    `${Math.round(Number(`${num}e${precision}`))}e${-precision}`,
+  ).toFixed(precision);
+  if (!keepTrailingZeroes) {
+    fixed = fixed.replace(/\.?0+$/, "");
+  }
+  return fixed;
+}

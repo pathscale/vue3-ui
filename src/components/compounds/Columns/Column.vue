230e6738e2d65192405b193c161c039b39889561
Lukas <kosha.industry@gmail.com>
2025-04-22 00:05:52 +0500

    refactor: add union type

--- src/components/compounds/Columns/Column.vue
+++ src/components/compounds/Columns/Column.vue
@@ -1,6 +1,28 @@
 <script setup lang="ts">
 defineProps<{
-  size?: string;
+  size?:
+    | "is-1"
+    | "is-2"
+    | "is-3"
+    | "is-4"
+    | "is-5"
+    | "is-6"
+    | "is-7"
+    | "is-8"
+    | "is-9"
+    | "is-10"
+    | "is-11"
+    | "is-12"
+    | "is-three-quarters"
+    | "is-two-thirds"
+    | "is-half"
+    | "is-one-third"
+    | "is-one-quarter"
+    | "is-full"
+    | "is-four-fifths"
+    | "is-three-fifths"
+    | "is-two-fifths"
+    | "is-one-fifth";
   offset?: string;
   narrow?: boolean;
   narrowBreakpoint?: string;

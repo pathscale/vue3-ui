6a99bd339e3738d6d18e9e81a304c2ea14c1cebf
Lukas <kosha.industry@gmail.com>
2025-04-22 04:51:20 +0500

    feat: add unrefElement.ts

---
+++ src/utils/unrefElement.ts
@@ -0,0 +1,29 @@
+import type { ComponentPublicInstance, MaybeRef, MaybeRefOrGetter } from "vue";
+import { toValue } from "vue";
+
+export type VueInstance = ComponentPublicInstance;
+export type MaybeElementRef<T extends MaybeElement = MaybeElement> =
+  MaybeRef<T>;
+export type MaybeComputedElementRef<T extends MaybeElement = MaybeElement> =
+  MaybeRefOrGetter<T>;
+export type MaybeElement =
+  | HTMLElement
+  | SVGElement
+  | VueInstance
+  | undefined
+  | null;
+
+export type UnRefElementReturn<T extends MaybeElement = MaybeElement> =
+  T extends VueInstance ? Exclude<MaybeElement, VueInstance> : T | undefined;
+
+/**
+ * Get the dom element of a ref of element or Vue component instance
+ *
+ * @param elRef
+ */
+export function unrefElement<T extends MaybeElement>(
+  elRef: MaybeComputedElementRef<T>,
+): UnRefElementReturn<T> {
+  const plain = toValue(elRef);
+  return (plain as VueInstance)?.$el ?? plain;
+}

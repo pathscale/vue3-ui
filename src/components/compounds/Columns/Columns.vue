d65e4e083522fd41612e9dcf9adbf8fea0ea42d5
Lukas <kosha.industry@gmail.com>
2025-04-22 00:38:19 +0500

    docs: define docs for gap

--- src/components/compounds/Columns/Columns.vue
+++ src/components/compounds/Columns/Columns.vue
@@ -3,6 +3,36 @@ defineProps<{
   mobile?: boolean;
   desktop?: boolean;
   gapless?: boolean;
+  /**
+   * Defines the column gap between items.
+   *
+   * You can specify a custom gap size using the following modifiers:
+   *   is-0, is-1, is-2, is-3, is-4, is-5, is-6, is-7, is-8
+   *
+   * You can also define responsive gaps for specific breakpoints
+   * by appending the breakpoint name to the modifier.
+   *
+   * Example usage:
+   * ```vue
+   * <v-columns
+   *   gap="is-2-mobile is-0-tablet is-3-desktop is-8-widescreen is-1-fullhd"
+   * >
+   *   ...
+   * </v-columns>
+   * ```
+   *
+   * Breakpoints:
+   * - mobile
+   * - tablet
+   * - desktop
+   * - widescreen
+   * - fullhd
+   *
+   * And special breakpoints for specific screen sizes:
+   * - tablet-only
+   * - desktop-only
+   * - widescreen-only
+   */
   gap?: string;
   multiline?: boolean;
   hcentered?: boolean;

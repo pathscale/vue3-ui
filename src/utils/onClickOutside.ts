d66cbac7ef730331c0cdec946e65ac312b930811
Lukas <kosha.industry@gmail.com>
2025-04-22 04:52:36 +0500

    feat: add onClickOutside.ts

---
+++ src/utils/onClickOutside.ts
@@ -0,0 +1,33 @@
+import { onBeforeUnmount, onMounted } from "vue";
+import { type MaybeElementRef, unrefElement } from "./unrefElement";
+
+export type OnClickOutsideHandler = (event: PointerEvent) => void;
+
+export function onClickOutside(
+  target: MaybeElementRef,
+  handler: OnClickOutsideHandler,
+) {
+  const listener = (event: Event) => {
+    const el = unrefElement(target);
+
+    if (event.target == null) {
+      return;
+    }
+
+    if (!el || el === event.target || event.composedPath().includes(el)) {
+      return;
+    }
+
+    // clicked outside
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    handler(event as any);
+  };
+
+  onMounted(() => {
+    window.addEventListener("click", listener);
+  });
+
+  onBeforeUnmount(() => {
+    window.removeEventListener("click", listener);
+  });
+}

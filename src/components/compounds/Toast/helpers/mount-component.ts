005868285d7ed557b28254b31edd8555c8e48e55
Lukas <kosha.industry@gmail.com>
2025-04-22 18:57:33 +0500

    refactor: typing

--- src/components/compounds/Toast/helpers/mount-component.ts
+++ src/components/compounds/Toast/helpers/mount-component.ts
@@ -1,12 +1,15 @@
-import { h, render } from "vue";
+import { type Component, type VNode, h, render } from "vue";
 
 const createElement = () =>
   typeof document !== "undefined" && document.createElement("div");
 
-const mount = (component, { props, children, element, app } = {}) => {
+const mount = (
+  component: Component,
+  { props, children, element, app } = {},
+) => {
   let el = element || createElement();
 
-  let vNode = h(component, props, children);
+  let vNode: VNode | null = h(component, props, children);
 
   if (app?._context) {
     vNode.appContext = app._context;

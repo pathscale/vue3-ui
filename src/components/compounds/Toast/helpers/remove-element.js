e4ef4b8097739c6b867565388f684c00421de8f7
Lukas <kosha.industry@gmail.com>
2025-04-22 18:18:21 +0500

    style: remove eslint-disable

--- src/components/compounds/Toast/helpers/remove-element.js
+++ src/components/compounds/Toast/helpers/remove-element.js
@@ -2,7 +2,6 @@ const removeElement = (el) => {
   if (typeof el.remove !== "undefined") {
     el.remove();
   } else {
-    // eslint-disable-next-line unicorn/prefer-node-remove -- ignore
     el.parentNode.removeChild(el);
   }
 };

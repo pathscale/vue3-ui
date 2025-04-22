884973ce2d48da3dc70f9fe86554ae33668aacc8
Lukas <kosha.industry@gmail.com>
2025-04-22 18:47:02 +0500

    refactor: typing

--- src/components/compounds/Toast/helpers/timer.ts
+++ src/components/compounds/Toast/helpers/timer.ts
@@ -1,11 +1,18 @@
+type Callback = () => void;
+
 // https://stackoverflow.com/a/3969760
 export default class Timer {
-  constructor(callback, delay) {
+  startedAt: number;
+  callback: Callback;
+  delay: number;
+  timer: number;
+
+  constructor(callback: Callback, delay: number) {
     this.startedAt = Date.now();
     this.callback = callback;
     this.delay = delay;
 
-    this.timer = setTimeout(callback, delay);
+    this.timer = window.setTimeout(callback, delay);
   }
 
   pause() {

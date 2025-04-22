f4ad8fd83f94efc7cb044bb75682232155669d16
Lukas <kosha.industry@gmail.com>
2025-04-22 19:55:19 +0500

    refactor: modern vue sfc style

--- src/components/primitives/Calendar/Calendar.vue
+++ src/components/primitives/Calendar/Calendar.vue
@@ -2,93 +2,75 @@
   <input ref="input" :type="type" />
 </template>
 
-<script>
+<script setup lang="ts">
 import { inject, onMounted, ref } from "vue";
 
-export default {
-  name: "VCalendar",
-  props: {
-    clearable: {
-      type: Boolean,
-    },
-    dialog: {
-      type: Boolean,
-    },
-    inline: {
-      type: Boolean,
-    },
-    range: {
-      type: Boolean,
-    },
-    options: {
-      type: Object,
-      default() {
-        return {};
-      },
-    },
-    type: {
-      type: String,
-      default: "datetime",
-    },
-    modelValue: [Date, Array],
+const props = withDefaults(
+  defineProps<{
+    clearable?: boolean;
+    dialog?: boolean;
+    inline?: boolean;
+    range?: boolean;
+    options?: object; // todo typed
+    type?: "date" | "time" | "datetime";
+    modelValue?: Date | Date[];
+  }>(),
+  {
+    options: () => ({}),
+    type: "datetime",
   },
-  emits: ["update:modelValue", "select"],
-  setup(props, { emit, slots }) {
-    const date = ref([null, null]);
-    const input = ref(null);
+);
 
-    onMounted(() => {
-      const bulmaCalendar = inject("$bulmaCalendar");
-      if (!bulmaCalendar) {
-        throw new Error(
-          "BulmaCalendar component requires the bulmaCalendar service to be provided",
-        );
-      }
-      // Set date
-      if (props.range) {
-        if (Array.isArray(props.modelValue)) {
-          date.value = props.modelValue;
-        }
-      } else {
-        date.value[0] = props.modelValue;
-      }
+const emit = defineEmits(["update:modelValue", "select"]);
 
-      const calendar = bulmaCalendar.attach(input.value, {
-        ...props.options,
-        displayMode: props.inline
-          ? "inline"
-          : props.dialog
-            ? "dialog"
-            : "default",
-        isRange: props.range,
-        showClearButton: props.clearable,
-        startDate: date.value[0],
-        startTime: date.value[0],
-        endDate: date.value[1],
-        endTime: date.value[1],
-      });
+const date = ref([null, null]);
+const input = ref(null);
 
-      calendar[0].on("save", (e) => {
-        date.value = [e.data.startDate, e.data.endDate];
+onMounted(() => {
+  const bulmaCalendar = inject("$bulmaCalendar");
+  if (!bulmaCalendar) {
+    throw new Error(
+      "BulmaCalendar component requires the bulmaCalendar service to be provided",
+    );
+  }
+  // Set date
+  if (props.range) {
+    if (Array.isArray(props.modelValue)) {
+      date.value = props.modelValue;
+    }
+  } else {
+    date.value[0] = props.modelValue;
+  }
 
-        if (props.range) {
-          emit("update:modelValue", date.value);
-          return;
-        }
+  const calendar = bulmaCalendar.attach(input.value, {
+    ...props.options,
+    displayMode: props.inline ? "inline" : props.dialog ? "dialog" : "default",
+    isRange: props.range,
+    showClearButton: props.clearable,
+    startDate: date.value[0],
+    startTime: date.value[0],
+    endDate: date.value[1],
+    endTime: date.value[1],
+  });
 
-        emit("update:modelValue", date.value[0]);
-      });
+  calendar[0].on("save", (e) => {
+    date.value = [e.data.startDate, e.data.endDate];
 
-      calendar[0].on("select", (e) => {
-        if (props.range) {
-          emit("select", [e.data.startDate, e.data.endDate]);
-          return;
-        }
+    if (props.range) {
+      emit("update:modelValue", date.value);
+      return;
+    }
 
-        emit("select", e.data.startDate);
-      });
-    });
-    return { input };
-  },
-};
+    emit("update:modelValue", date.value[0]);
+  });
+
+  calendar[0].on("select", (e) => {
+    if (props.range) {
+      emit("select", [e.data.startDate, e.data.endDate]);
+      return;
+    }
+
+    emit("select", e.data.startDate);
+  });
+});
 </script>

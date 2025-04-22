2d6680e425468e00ec12d3a9ef801375f7c311c9
Lukas <kosha.industry@gmail.com>
2025-04-22 07:52:48 +0500

    refactor(compounds): modern vue sfc style

--- src/components/compounds/Steps/StepItem.vue
+++ src/components/compounds/Steps/StepItem.vue
@@ -1,41 +1,30 @@
-<script>
+<script setup lang="ts">
 import { computed, ref, watch } from "vue";
 import { addToStore, useStore } from "./Steps.vue";
 
-export default {
-  name: "VStep",
-  props: {
-    // eslint-disable-next-line vue/no-unused-properties -- used
-    title: String,
-    // eslint-disable-next-line vue/no-unused-properties -- used
-    subtitle: String,
-    // eslint-disable-next-line vue/no-unused-properties -- used
-    marker: String,
-    // eslint-disable-next-line vue/no-unused-properties -- used
-    disabled: Boolean,
-    // eslint-disable-next-line vue/no-unused-properties -- used
-    clickable: Boolean,
+defineProps<{
+  title?: string;
+  subtitle?: string;
+  marker?: string;
+  disabled?: boolean;
+  clickable?: boolean;
+}>();
+
+const content = ref(null);
+const tabs = useStore();
+const id = JSON.parse(JSON.stringify(tabs.value.tabs)).length;
+
+addToStore({ ...props, id });
+
+watch(
+  () => tabs.value.tabs.findIndex((tab) => tab.id === id),
+  (index) => {
+    tabs.value.tabs.splice(index, 1, { id, ...props });
   },
-  setup(props, { emit }) {
-    const content = ref(null);
-    const tabs = useStore();
-    const id = JSON.parse(JSON.stringify(tabs.value.tabs)).length;
+  { immediate: true },
+);
 
-    addToStore({ ...props, id });
-
-    watch(
-      () => tabs.value.tabs.findIndex((tab) => tab.id === id),
-      (index) => {
-        tabs.value.tabs.splice(index, 1, { id, ...props });
-      },
-      { immediate: true },
-    );
-
-    const isActiveTab = computed(() => tabs.value.activeTab === id);
-
-    return { content, isActiveTab };
-  },
-};
+const isActiveTab = computed(() => tabs.value.activeTab === id);
 </script>
 
 <template>

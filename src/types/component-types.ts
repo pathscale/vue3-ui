faa78908e216cd6c9ea0dbe393594f897686ec78
Lukas <kosha.industry@gmail.com>
2025-04-22 07:26:44 +0500

    feat: single file for all component shared types

--- src/types/component-types.ts
+++ src/types/component-types.ts
@@ -21,3 +21,17 @@ export interface CheckRadioProps {
   name?: string;
   size?: "is-small" | "is-medium" | "is-large";
 }
+
+/**
+ * This object used for every page item inside pagination
+ *
+ * https://vue3.dev/documentation/pagination
+ */
+export interface PaginationPage {
+  number?: number;
+  isCurrent?: boolean;
+  click?: (e: Event) => void;
+  disabled?: boolean;
+  class?: string;
+  "aria-label"?: string;
+}

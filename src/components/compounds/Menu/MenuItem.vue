493b39ed5d1b9d1ef68e42e6251aacd95ac94836
AbsoluteC2H6O <alfredofp63@gmail.com>
2025-04-23 00:39:50 -0400

    fix(menu): properly handle submenu expansion
    - Update menu item click handlers
    - Fix aria attributes for accessibility

--- src/components/compounds/Menu/MenuItem.vue
+++ src/components/compounds/Menu/MenuItem.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useTemplateRef } from "vue";
+import { ref, watchEffect } from "vue";
 
 defineOptions({
   inheritAttrs: false,
@@ -12,51 +12,65 @@ const props = withDefaults(
     icon?: string;
     tag?: string;
     ariaRole?: string;
+    active?: boolean;
+    expanded?: boolean;
   }>(),
   {
     tag: "a",
     ariaRole: "menuitem",
+    active: false,
+    expanded: false,
   },
 );
 
-const active = defineModel<boolean>("active");
-const expanded = defineModel<boolean>("expanded");
-const content = useTemplateRef<HTMLElement>("content");
+const emit = defineEmits<{
+  (e: "update:expanded", value: boolean): void;
+  (e: "update:active", value: boolean): void;
+}>();
 
-const onClick = () => {
-  // TODO Disable previous active item
-  if (props.disabled) return;
+const active = ref(props.active);
+const expanded = ref(props.expanded);
+const content = ref<HTMLElement | null>(null);
 
-  expanded.value = !expanded.value;
-  active.value = true;
+// Sync with props
+watchEffect(() => {
+  active.value = props.active;
+  expanded.value = props.expanded;
+});
+
+const onClick = (event: Event) => {
+  if (props.disabled) {
+    event.preventDefault();
+    return;
+  }
+
+  // Toggle expansion state
+  const newExpanded = !expanded.value;
+  expanded.value = newExpanded;
+
+  // Emit changes to parent
+  emit("update:expanded", newExpanded);
 };
 </script>
 
 <template>
-  <li class="li" :role="ariaRole" ref="content">
-    <component
-      :is="tag"
-      v-bind="$attrs"
-      :class="{
-        'is-flex': icon,
-        'is-active': active,
-        'is-disabled': disabled
-      }"
-      @click="onClick">
-      <span v-if="icon" class="pr-2">{{ icon }} </span>
+  <li class="li" :role="ariaRole" ref="content" :aria-expanded="expanded">
+    <component :is="tag" v-bind="$attrs" :class="{
+      'is-flex': icon,
+      'is-active': active,
+      'is-disabled': disabled
+    }" :aria-disabled="disabled" @click="onClick" @keydown.enter="onClick">
+      <span v-if="icon" class="pr-2">{{ icon }}</span>
       <span v-if="label">{{ label }}</span>
-      <slot
-        v-else
-        name="label"
-        :expanded="expanded"
-        :active="active" />
+      <slot v-else name="label" :expanded="expanded" :active="active" />
     </component>
+
     <template v-if="$slots.default">
-      <!-- <transition name="slide"> -->
-      <ul class="ul" v-show="expanded">
-        <slot />
-      </ul>
-      <!-- </transition> -->
+      <transition name="slide">
+        <ul v-show="expanded" class="ul" role="menu">
+          <slot />
+        </ul>
+      </transition>
     </template>
   </li>
 </template>
